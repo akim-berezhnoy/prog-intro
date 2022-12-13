4e3b5d1772093b816bd8fcd0cbcd75bd5fc8e6c7
akim-berezhnoy <berezhnoy.akim@gmail.com>
2022-12-14 01:43:44 +0300

    +fixed main function

--- java-solutions/expression/Main.java
+++ java-solutions/expression/Main.java
@@ -1,19 +1,16 @@
 package expression;
 
-import expression.parser.ExpressionParser;
-
 public class Main {
     public static void main(String[] args) {
-//        Expression exp = new Add(
-//                             new Subtract(
-//                                     new Multiply(
-//                                             new Variable("x"),
-//                                             new Variable("x")),
-//                                     new Multiply(
-//                                             new Const(2),
-//                                             new Variable("x"))),
-//                             new Const(1));
-//        System.out.println(exp.evaluate(Integer.parseInt(args[0])));
-        System.out.println(new ExpressionParser().parse("- 0"));
+        Expression exp = new Add(
+                             new Subtract(
+                                     new Multiply(
+                                             new Variable("x"),
+                                             new Variable("x")),
+                                     new Multiply(
+                                             new Const(2),
+                                             new Variable("x"))),
+                             new Const(1));
+        System.out.println(exp.evaluate(Integer.parseInt(args[0])));
     }
 }
